3fa4ee1daf9ca030f91506ca8e67ded091536fe8
Bodiiiick <bohdan22810@gmail.com>
2025-05-30 15:52:38 +0200

    Edit User.java

--- src/Library/model/User.java
+++ src/Library/model/User.java
@@ -1,4 +1,42 @@
 package Library.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String username;
+    private String password;
+    private Role role;
+    private List<String> borowedBooks;
+
+    public User(String username, String password, Role role, List<String> borowedBooks) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.borowedBooks = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public List<String> getBorowedBooks() {
+        return borowedBooks;
+    }
+
+    public void borrowBook(String bookTitle){
+        borowedBooks.add(bookTitle);
+    }
+
+    public void returnBook(String bookTitle){
+        borowedBooks.remove(bookTitle);
+    }
 }
