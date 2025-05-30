58c567810d7b84178d40650a02cce2975199bb48
Bodiiiick <bohdan22810@gmail.com>
2025-05-30 16:21:38 +0200

    added login metod

--- src/Library/service/AuthService.java
+++ src/Library/service/AuthService.java
@@ -1,4 +1,23 @@
 package Library.service;
 
+import Library.model.Role;
+import Library.model.User;
+
+import java.util.ArrayList;
+
 public class AuthService {
+    public User login(String username , String password){
+        if (username.equals("admin") && password.equals("admin")){
+            return new User("admin", "admin", Role.ADMIN, new ArrayList<>());
+        }
+        if (username.equals("user") && password.equals("user")) {
+            return new User("user", "user", Role.USER, new ArrayList<>());
+        }
+
+        return null;
+    }
+
+//    public void logout(){
+//
+//    }
 }
